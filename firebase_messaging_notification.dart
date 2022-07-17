de9d55957f7c6aacc730532c6c81f3eb7ea4e91e
Marcela <marcelacrv@icloud.com>
2022-07-17 19:05:56 -0300

    Create firebase_messaging_notification.dart

---
+++ firebase_messaging_notification.dart
@@ -0,0 +1,44 @@
+import 'package:app_adove/app/data/notification/custom_notification.dart';
+import 'package:firebase_messaging/firebase_messaging.dart';
+import 'package:get/get.dart';
+
+class FirebaseMessagingNotification {
+  static FirebaseMessagingNotification get to => Get.find<FirebaseMessagingNotification>();
+  
+  final NotificationService _notificationService;
+
+  FirebaseMessagingNotification(this._notificationService);
+
+  Future<void> initialize() async {
+    await FirebaseMessaging.instance
+        .setForegroundNotificationPresentationOptions(
+      badge: true,
+      sound: true,
+      alert: true,
+    );
+    getDeviceFirebaseToken();
+    _onMessage();
+  }
+
+  getDeviceFirebaseToken() async {
+    final firebaseToken = await FirebaseMessaging.instance.getToken();
+  }
+
+  _onMessage() {
+    FirebaseMessaging.onMessage.listen((RemoteMessage message) {
+      RemoteNotification? notification = message.notification;
+      AndroidNotification? android = message.notification?.android;
+
+      if (notification != null && android != null) {
+        _notificationService.showNotification(
+          CustomNotification(
+            id: android.hashCode,
+            title: notification.title,
+            body: notification.body,
+            payload: message.data['route'] ?? '',
+          ),
+        );
+      }
+    });
+  }
+}
